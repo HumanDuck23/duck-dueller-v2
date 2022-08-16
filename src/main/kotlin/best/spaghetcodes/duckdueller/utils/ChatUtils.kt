59f5aa53cbc20be064c7ccf0ea78e9f92e369f2f
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-08-16 15:03:33 +0200

    created ChatUtils object

---
+++ src/main/kotlin/best/spaghetcodes/duckdueller/utils/ChatUtils.kt
@@ -0,0 +1,46 @@
+package best.spaghetcodes.duckdueller.utils
+
+import best.spaghetcodes.duckdueller.DuckDueller
+import net.minecraft.util.ChatComponentText
+import net.minecraft.util.EnumChatFormatting
+
+object ChatUtils {
+
+    fun removeFormatting(text: String): String{
+        var t = ""
+        var skip = false
+        for (i in text.indices) {
+            if (!skip) {
+                if (text[i] == '§') {
+                    skip = true
+                } else {
+                    t += text[i]
+                }
+            } else {
+                skip = false
+            }
+        }
+        return t
+    }
+
+    fun sendAsPlayer(message: String) {
+        if (DuckDueller.mc.thePlayer != null) {
+            DuckDueller.mc.thePlayer.sendChatMessage(message)
+        }
+    }
+
+    fun info(message: String) {
+        sendChatMessage("${EnumChatFormatting.GOLD}[${EnumChatFormatting.LIGHT_PURPLE}Duck ${EnumChatFormatting.DARK_PURPLE}Dueller${EnumChatFormatting.GOLD}] ${EnumChatFormatting.WHITE}$message")
+    }
+
+    fun error(message: String) {
+        sendChatMessage("${EnumChatFormatting.GOLD}[${EnumChatFormatting.LIGHT_PURPLE}Duck ${EnumChatFormatting.DARK_PURPLE}Dueller${EnumChatFormatting.GOLD}] ${EnumChatFormatting.RED}$message")
+    }
+
+    private fun sendChatMessage(message: String) {
+        if (DuckDueller.mc.thePlayer != null) {
+            DuckDueller.mc.thePlayer.addChatMessage(ChatComponentText(message))
+        }
+    }
+
+}
