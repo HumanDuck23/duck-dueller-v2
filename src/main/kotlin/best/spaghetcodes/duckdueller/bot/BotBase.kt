fd250839e056cd683239e2e30b63ce01c7dc2715
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-08-16 13:53:16 +0200

    created overridable methods in BotBase

--- src/main/kotlin/best/spaghetcodes/duckdueller/bot/BotBase.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/BotBase.kt
@@ -17,4 +17,55 @@ open class BotBase(val queueCommand: String, val quickRefresh: Int = 10000) {
         toggled = !toggled
     }
 
+    /********
+     * Methods to override
+     ********/
+
+    /**
+     * Called when the bot attacks the opponent
+     * Triggered by the damage sound, not the clientside attack event
+     */
+    protected open fun onAttack() {}
+
+    /**
+     * Called when the bot is attacked
+     * Triggered by the damage sound, not the clientside attack event
+     */
+    protected open fun onAttacked() {}
+
+    /**
+     * Called when the game starts
+     */
+    protected open fun onGameStart() {}
+
+    /**
+     * Called when the game ends
+     */
+    protected open fun onGameEnd() {}
+
+    /**
+     * Called when the bot joins a game
+     */
+    protected open fun onJoinGame() {}
+
+    /**
+     * Called before the game starts (1s)
+     */
+    protected open fun beforeStart() {}
+
+    /**
+     * Called before the bot leaves the game (dodge)
+     */
+    protected open fun beforeLeave() {}
+
+    /**
+     * Called when the opponent entity is found
+     */
+    protected open fun onFoundOpponent() {}
+
+    /**
+     * Called every tick
+     */
+    protected open fun onTick() {}
+
 }
