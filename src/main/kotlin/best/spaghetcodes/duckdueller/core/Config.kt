1eb7e650c54afc3727da78935e423c134f4a3e2e
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-08-16 12:06:24 +0200

    created combat config category

--- src/main/kotlin/best/spaghetcodes/duckdueller/core/Config.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/core/Config.kt
@@ -47,6 +47,76 @@ class Config : Vigilant(File(DuckDueller.configLocation)) {
     )
     val webhookURL = ""
 
+    /*
+        COMBAT
+     */
+
+    @Property(
+        type = PropertyType.NUMBER,
+        name = "Min CPS",
+        description = "The minimum CPS that the bot will be clicking at.",
+        category = "Combat",
+        min = 6,
+        max = 15,
+        increment = 1
+    )
+    val minCPS = 10
+
+    @Property(
+        type = PropertyType.NUMBER,
+        name = "Max CPS",
+        description = "The maximum CPS that the bot will be clicking at.",
+        category = "Combat",
+        min = 9,
+        max = 18,
+        increment = 1
+    )
+    val maxCPS = 10
+
+    @Property(
+        type = PropertyType.NUMBER,
+        name = "Look Speed",
+        description = "How fast the bot can look around (lower number = less snappy, slightly less accurate when teleporting)",
+        category = "Combat",
+        min = 5,
+        max = 15,
+        increment = 1
+    )
+    val lookSpeed = 10
+
+    @Property(
+        type = PropertyType.NUMBER,
+        name = "Look Randomization",
+        description = "How much randomization should happen when looking (higher number = more jittery aim)",
+        category = "Combat",
+        min = 5,
+        max = 15,
+        increment = 1
+    )
+    val lookRand = 10
+
+    @Property(
+        type = PropertyType.NUMBER,
+        name = "Max Look Distance",
+        description = "How close the opponent has to be before the bot starts tracking them",
+        category = "Combat",
+        min = 120,
+        max = 180,
+        increment = 5
+    )
+    val maxDistanceLook = 150
+
+    @Property(
+        type = PropertyType.NUMBER,
+        name = "Max Attack Distance",
+        description = "How close the opponent has to be before the bot starts attacking them",
+        category = "Combat",
+        min = 5,
+        max = 15,
+        increment = 150
+    )
+    val maxDistanceAttack = 10
+
     init {
         addDependency("webhookURL", "sendWebhookMessages")
 
