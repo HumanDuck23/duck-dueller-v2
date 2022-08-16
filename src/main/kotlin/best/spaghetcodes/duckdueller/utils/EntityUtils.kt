bcc2b3d6ef1a31f5430de074eac9798bd0382da4
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-08-16 16:30:50 +0200

    created crossHairDistance, getDistanceNoY and get2dLookVec methods

--- src/main/kotlin/best/spaghetcodes/duckdueller/utils/EntityUtils.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/utils/EntityUtils.kt
@@ -1,7 +1,12 @@
 package best.spaghetcodes.duckdueller.utils
 
 import best.spaghetcodes.duckdueller.DuckDueller
+import net.minecraft.entity.Entity
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.MathHelper
+import net.minecraft.util.Vec3
+import kotlin.math.cos
+import kotlin.math.sin
 
 object EntityUtils {
 
@@ -32,6 +37,27 @@ object EntityUtils {
         } else {
             false
         }
+    }
+
+    fun crossHairDistance(yaw: Float, pitch: Float, player: EntityPlayer): Float {
+        val nYaw = yaw - player.rotationYaw - yaw
+        val nPitch = pitch - player.rotationPitch - pitch
+        return MathHelper.sqrt_float(nYaw * nYaw + nPitch * nPitch)
+    }
+
+    fun getDistanceNoY(player: EntityPlayer?, target: Entity?): Float {
+        return if (target == null || player == null) {
+            0f
+        } else {
+            val diffX = player.posX - target.posX
+            val diffZ = player.posZ - target.posZ
+            MathHelper.sqrt_double(diffX * diffX + diffZ * diffZ).toFloat()
+        }
+    }
+
+    fun get2dLookVec(entity: Entity): Vec3 {
+        val yaw = ((entity.rotationYaw + 90)  * Math.PI) / 180
+        return Vec3(cos(yaw), 0.0, sin(yaw))
     }
 
 }
