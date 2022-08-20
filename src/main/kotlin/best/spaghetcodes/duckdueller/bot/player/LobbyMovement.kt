edd114f7aaf8db0560839a0d2ae2926720a6f3a0
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-08-20 12:45:47 +0200

    created LobbyMovement object

---
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/LobbyMovement.kt
@@ -0,0 +1,72 @@
+package best.spaghetcodes.duckdueller.bot.player
+
+import best.spaghetcodes.duckdueller.DuckDueller
+import best.spaghetcodes.duckdueller.bot.StateManager
+import best.spaghetcodes.duckdueller.utils.RandomUtils
+import best.spaghetcodes.duckdueller.utils.TimeUtils
+import best.spaghetcodes.duckdueller.utils.WorldUtils
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
+import java.util.Timer
+
+object LobbyMovement {
+
+    private var tickYawChange = 0f
+    private var intervals: ArrayList<Timer?> = ArrayList()
+
+    fun sumo() {
+        val opt = RandomUtils.randomIntInRange(0, 1)
+        when (opt) {
+            0 -> sumo1()
+            1 -> twerk()
+        }
+    }
+
+    fun stop() {
+        Movement.clearAll()
+        tickYawChange = 0f
+        intervals.forEach { it?.cancel() }
+    }
+
+    private fun sumo1() {
+        if (DuckDueller.mc.thePlayer != null) {
+            val left = RandomUtils.randomBool()
+
+            tickYawChange = if (left) -8.5f else 8.5f
+            TimeUtils.setTimeout(fun () {
+                Movement.startForward()
+                Movement.startSprinting()
+                TimeUtils.setTimeout(fun () {
+                    Movement.startJumping()
+                }, RandomUtils.randomIntInRange(400, 800))
+                intervals.add(TimeUtils.setInterval(
+                    fun () {
+                        tickYawChange = if (WorldUtils.airInFront(DuckDueller.mc.thePlayer, 7f)) {
+                            RandomUtils.randomDoubleInRange(if (left) 4.5 else -4.5, if (left) 7.0 else -7.0).toFloat()
+                        } else {
+                            0f
+                        }
+                           }, 0, RandomUtils.randomIntInRange(50, 100)))
+            }, RandomUtils.randomIntInRange(100, 250))
+        }
+    }
+
+    private fun twerk() {
+        intervals.add(TimeUtils.setInterval(
+            fun () {
+                if (Movement.sneaking()) {
+                    Movement.stopSneaking()
+                } else {
+                    Movement.startSneaking()
+                }
+        }, RandomUtils.randomIntInRange(500, 900), RandomUtils.randomIntInRange(200, 500)))
+    }
+
+    @SubscribeEvent
+    fun onTick(ev: ClientTickEvent) {
+        if (DuckDueller.bot?.toggled() == true && tickYawChange != 0f && DuckDueller.mc.thePlayer != null && StateManager.state != StateManager.States.PLAYING) {
+            DuckDueller.mc.thePlayer.rotationYaw += tickYawChange
+        }
+    }
+
+}
