489d1077c9bb53fdbf29afb349d3ab25a94e45d0
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-09-08 16:05:41 +0200

    proper error messages and error handling for hypixel api errors

--- src/main/kotlin/best/spaghetcodes/duckdueller/utils/HttpUtils.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/utils/HttpUtils.kt
@@ -2,6 +2,7 @@ package best.spaghetcodes.duckdueller.utils
 
 import best.spaghetcodes.duckdueller.DuckDueller
 import com.google.gson.JsonObject
+import java.io.IOException
 import java.net.URL
 
 
@@ -10,9 +11,20 @@ object HttpUtils {
     fun getPlayerStats(uuid: String): JsonObject? {
         val key = DuckDueller.config?.apiKey
         if (key != null && key != "") {
-            val url = URL("https://api.hypixel.net/player?key=${key}&uuid=$uuid")
-            val json = url.readText()
-            return DuckDueller.gson.fromJson(json, JsonObject::class.java)
+            return try {
+                val url = URL("https://api.hypixel.net/player?key=${key}&uuid=$uuid")
+                val json = url.readText()
+                DuckDueller.gson.fromJson(json, JsonObject::class.java)
+            } catch (e: IOException) {
+                val msg = e.message
+                val rx = Regex("HTTP response code: (\\d+)")
+                val match = rx.find(msg!!)
+                if (match != null) {
+                    val code = match.groupValues[1].toInt()
+                    ChatUtils.info("Error getting stats for $uuid: $code")
+                }
+                null
+            }
         } else {
             return null
         }
