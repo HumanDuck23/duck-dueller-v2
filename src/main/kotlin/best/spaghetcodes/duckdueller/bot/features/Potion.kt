c1dde6546998866a6cd9e9b0b9309d207af1e534
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-10-08 15:45:21 +0200

    added drinking potions, stop left clicking when splashing

--- src/main/kotlin/best/spaghetcodes/duckdueller/bot/features/Potion.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/features/Potion.kt
@@ -1,5 +1,6 @@
 package best.spaghetcodes.duckdueller.bot.features
 
+import best.spaghetcodes.duckdueller.DuckDueller
 import best.spaghetcodes.duckdueller.bot.player.Inventory
 import best.spaghetcodes.duckdueller.bot.player.Mouse
 import best.spaghetcodes.duckdueller.utils.ChatUtils
@@ -14,6 +15,7 @@ interface Potion {
     fun useSplashPotion(damage: Int, run: Boolean, facingAway: Boolean) {
         lastPotion = System.currentTimeMillis()
         fun pot(dmg: Int) {
+            Mouse.stopLeftAC()
             if (Inventory.setInvItemByDamage(dmg)) {
                 ChatUtils.info("About to splash $dmg")
                 TimeUtils.setTimeout(fun() {
@@ -35,6 +37,37 @@ interface Potion {
                         }, r + RandomUtils.randomIntInRange(80, 120))
                     }, RandomUtils.randomIntInRange(100, 200))
                 }, RandomUtils.randomIntInRange(50, 100))
+            }
+        }
+
+        if (run && !facingAway) {
+            Mouse.setUsingProjectile(false)
+            Mouse.setRunningAway(true)
+            TimeUtils.setTimeout(fun() {
+                pot(damage)
+            }, RandomUtils.randomIntInRange(300, 500))
+        } else {
+            pot(damage)
+        }
+    }
+
+    fun usePotion(damage: Int, run: Boolean, facingAway: Boolean) {
+        fun pot(dmg: Int) {
+            Mouse.stopLeftAC()
+            if (Inventory.setInvItemByDamage(dmg)) {
+                ChatUtils.info("About to use $dmg")
+                TimeUtils.setTimeout(fun () {
+                    val r = RandomUtils.randomIntInRange(1900, 2050)
+                    Mouse.rClick(r)
+                    TimeUtils.setTimeout(fun () {
+                        Inventory.setInvItem("sword")
+                        TimeUtils.setTimeout(fun() {
+                            Mouse.setRunningAway(false)
+                        }, RandomUtils.randomIntInRange(500, 700))
+                    }, r + RandomUtils.randomIntInRange(80, 120))
+                }, RandomUtils.randomIntInRange(200, 400))
+            } else {
+                ChatUtils.error("No $dmg potion in inventory")
             }
         }
 
